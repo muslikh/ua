d4dec59ef0a174f34a3aa0e9c0a33105fdfdf325
Muhammad Muslikh <muslikh32@gmail.com>
2026-07-31 09:48:43 +0700

    Initial commit with Copy Button

--- app/src/main/java/com/example/cekuseragent/MainActivity.java
+++ app/src/main/java/com/example/cekuseragent/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.cekuseragent;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,10 +16,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView tvUserAgent = findViewById(R.id.tvUserAgent);
+        Button btnCopy = findViewById(R.id.btnCopy);
         
         // Ambil User Agent Android murni (Dalvik)
         String userAgent = System.getProperty("http.agent");
         
         tvUserAgent.setText("User Agent HP Anda:\n\n" + userAgent);
+
+        btnCopy.setOnClickListener(v -> {
+            ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("User Agent", userAgent);
+            clipboard.setPrimaryClip(clip);
+            Toast.makeText(MainActivity.this, "User Agent berhasil di-copy!", Toast.LENGTH_SHORT).show();
+        });
     }
 }
